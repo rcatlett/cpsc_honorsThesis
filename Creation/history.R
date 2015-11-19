1c5596c70f876e4cbd2889fd79ac72081fd540f3
catlett.rc <catlett.rc@gmail.com>
2015-11-19 12:11:27 -0500

    Adding plots and plot commands

--- Creation/history.R
+++ Creation/history.R
@@ -10,10 +10,21 @@ combined <- tbl_df(combined)
 
 avgEff <- combined %>% group_by(Player) %>% summarise(avgEFF = mean(EFF), years = mean(nYears)) 
 
+# Removes NCAA players from NBA
 myvars <- NBA$Player %in% NCAA$Player
 noCollege <- NBA[!myvars,]
 noCollegePlayers <- unique(noCollege$Player)
 
+#Groups the nocollege with their efficiency
 noCoEff <- noCollege %>% group_by(Player) %>% summarise(avgEFF = mean(EFF))
-plot(avgEff$avgEFF, avgEff$years, main="College Years over EFF in NBA")
-boxplot(noCoEff$avgEFF, main="Average EFF in NBA, No College")
+
+#Sets the graphical environment
+par(mar=c(1,1,1,1))
+par(mfrow=c(3,2))
+#Prints the 6 graphs
+boxplot(noCoEff$avgEFF, main="No College", ylim = c(-5,35))
+boxplot(avgEff[avgEff$year == 1,2], main = "1 Year College", ylim = c(-5,35))
+boxplot(avgEff[avgEff$year == 2,2], main = "2 Years College", ylim = c(-5,35))
+boxplot(avgEff[avgEff$year == 3,2], main = "3 Years College", ylim = c(-5,35))
+boxplot(avgEff[avgEff$year == 4,2], main = "4 Years College", ylim = c(-5,35))
+boxplot(avgEff[avgEff$year == 5,2], main = "5 Years College", ylim = c(-5,35))
