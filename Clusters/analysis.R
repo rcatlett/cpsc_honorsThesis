6e1b4c97bd07c68b99c6c6871c2545e7381eeddf
catlett.rc <catlett.rc@gmail.com>
2016-04-06 23:35:18 -0400

    Updated run files and added data files

--- Clusters/analysis.R
+++ Clusters/analysis.R
@@ -3,31 +3,32 @@ type <- readline("Enter what type of analysis you want to see: (Average, Max, Ye
 for (i in seq(1:num_clusters)) {
   fresh <- eff_frame[eff_frame$Cluster==i & eff_frame$CollegeYears==1,]
   other <- eff_frame[eff_frame$Cluster==i & eff_frame$CollegeYears!=1,]
-  
+  cat(i)
   if (type == "YearsToMax") {
-    # Normalize since some people will have a max their first and only year
-    
+    fresh <- fresh[, type] / fresh[,"NumYears"]
+    other <- other[,c(type,"CollegeYears", "NumYears")]
+    other$"YearsToMax" <- other$"YearsToMax"/other$"NumYears"
+    other <- other[,-3]
+    values <- eff_frame[,"YearsToMax"]/eff_frame[,"NumYears"]
   }
   else {
-    cat(i)
     fresh <- fresh[,type]
     other <- other[,c(type, "CollegeYears")]
-    title = paste(i, "-", type, sep=" ")
+    title = paste(i, "-", type, sep=" ") 
     values <- eff_frame[,type]
-    plot(rep(1, length(fresh)), fresh, main=title, xlim=c(1,5), col="red", ylim=c(min(values), max(values)))
-    points(other[, "CollegeYears"], other[,type], col="blue")
-    if (length(fresh) > 1) {
-      t.results <- t.test(fresh, other[,type])
-      cat(": P-Value - ", t.results$p.value, " and Confidence Interval - ", t.results$conf.int)
-      cat("\n")
-      if (t.results$p.value > 0.05) {
-        cat("There is a statistial difference.\n")
-      }
+  }
+  plot(rep(1, length(fresh)), fresh, main=title, xlim=c(1,5), col="red", ylim=c(min(values), max(values)))
+  points(other[, "CollegeYears"], other[,type], col="blue")
+  if (length(fresh) > 1) {
+    t.results <- t.test(fresh, other[,type])
+    cat(": P-Value - ", t.results$p.value, " and Confidence Interval - ", t.results$conf.int)
+    cat("\n")
+    if (t.results$p.value < 0.05) {
+      # Is this the right direction?
+      cat("There is a statistial difference.\n")
     }
-    else {
-      cat(": Not enough freshmen.\n")
-    }
-    
-    
+  }
+  else {
+    cat(": Not enough freshmen.\n")
   }
 }
--- Clusters/eff_frame.R
+++ Clusters/eff_frame.R
@@ -1,20 +1,22 @@
 # Run after clusters_final.R
 
-eff_frame <- data.frame(matrix(ncol=7))
-colnames(eff_frame) <- c("Player", "Cluster", "Average", "Max", "YearsToMax", "NumYears", "AvgTopThree")
-l <- 0                       
+eff_frame <- data.frame(matrix(ncol=9))
+colnames(eff_frame) <- c("Player", "Cluster", "Average", "Max", "YearsToMax", "NumYears", "CollegeYears", "AvgTopThree", "YearsOverAvg")
+l <- 1                      
 for (player in names(clusters)) {
-  eff_frame[l+1,"Player"] <- player
+  eff_frame[l,"Player"] <- player
   effs <- NBA[NBA$Player==player, "EFF"]
-  eff_frame[l+1,"Cluster"] <- clusters[player]
-  
-  eff_frame[l+1, "Average"] <- colMeans(effs)
-  eff_frame[l+1, "Max"] <-max(effs)
-  eff_frame[l+1, "YearsToMax"] <- which.max(effs$EFF)
-  eff_frame[l+1, "NumYears"] <- nrow(effs)
-  eff_frame[l+1, "AvgTopThree"] <- mean(sort(effs$EFF)[1:min(3, length(effs$EFF))])
+  eff_frame[l,"Cluster"] <- clusters[player]
+  eff_frame[l, "YearsOverAvg"] <- nrow(effs[effs$EFF > colMeans(effs),])
+  eff_frame[l, "CollegeYears"] <- nrow(NCAA[NCAA$Player==player,])
+  eff_frame[l, "Average"] <- colMeans(effs)
+  eff_frame[l, "Max"] <-max(effs)
+  eff_frame[l, "YearsToMax"] <- which.max(effs$EFF)
+  eff_frame[l, "NumYears"] <- nrow(effs)
+  eff_frame[l, "AvgTopThree"] <- mean(sort(effs$EFF)[1:min(3, length(effs$EFF))])
   
   l <- l + 1
   
 }
 
+rm(list=c("effs","l","player"))
